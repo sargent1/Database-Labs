01f3eca3871bca21e3bf41e48896d868016c4e7b
sargent1 <connor.sargent1@marist.edu>
2015-03-10 18:00:02 -0400

    lab 3
    lab 3 queries

---
+++ lab3.sql
@@ -0,0 +1,41 @@
+﻿select ordno, dollars
+from orders;
+
+select name, city
+from agents 
+where name = 'Smith'
+
+select pid, name, priceUSD 
+from products 
+where quantity > 200,000
+
+select name, city, customers 
+from products 
+where city = 'Dallas'
+
+select name
+from agents 
+where city != 'New York', 'Tokyo'
+
+
+select *
+from products
+where != 'Dallas', 'Duluth', priceUSD > 1.00
+
+select *
+from orders
+where mon = 'jan', 'may'
+
+select *
+from orders
+where mon = 'feb', dollars > 500
+
+select *
+from orders
+where cid = c005
+
+
+
+
+
+
