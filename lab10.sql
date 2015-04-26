55ca55f48ee7d5fb058b1629c100457b2fb287f7
sargent1 <connor.sargent1@marist.edu>
2015-04-26 16:51:12 -0400

    lab 10
    lab 10

---
+++ lab10.sql
@@ -0,0 +1,42 @@
+﻿--Connor Sargent
+--Lab 10
+
+--1)
+
+create or replace function PreReqsFor(int, REFCURSOR) returns refcursor as 
+$$
+declare
+   course      int       := $1;
+   resultset   REFCURSOR := $2;
+begin
+   open resultset for 
+      select prereqnum
+      from prerequisites
+      where courseNum = course;
+   return resultset;
+end;
+$$ 
+language plpgsql;
+
+select PreReqsFor(308, 'results');
+Fetch all from results;
+
+--2) 
+
+create or replace function IsPreReqFor(int, REFCURSOR) returns refcursor as 
+$$
+declare
+   prereq      int       := $1;
+   resultset   REFCURSOR := $2;
+begin
+   open resultset for 
+      select coursenum
+      from prerequisites
+      where prereq =  prereqnum;
+   return resultset;
+end;
+$$ 
+language plpgsql;
+
+select IsPreReqFor(220, 'results');
+Fetch all from results;
